d386b22dfd2bfbd052484a19f986b0ea2d5cf41e
Terrence Tang <thtang28@gmail.com>
2018-10-03 15:15:37 -0700

    finish judge circle

--- judge_circle.rb
+++ judge_circle.rb
@@ -1,5 +1,19 @@
 def judge_circle(string)
-
+  x = 0
+  y = 0
+  string.each_char do |ch|
+    case ch
+    when "R"
+      x += 1
+    when "L"
+      x -= 1
+    when "U"
+      y += 1
+    when "D"
+      y -= 1
+    end
+  end
+  x == 0 && y == 0
 end
 
 puts judge_circle("UD") == true
